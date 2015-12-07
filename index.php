c21eac14a7375b55076edb8e7163b4f654fe1d30
linguochi <linguochi@weidian.com>
2015-12-07 19:04:50 +0800

    init

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>WebUploader演示</title>
+    <link rel="stylesheet" type="text/css" href="css/webuploader.css"/>
+</head>
+<body>
+<div id="wrapper">
+    <div id="container">
+        <!--头部，相册选择和格式选择-->
+        <div id="uploader" class="wu-example">
+            <!--用来存放文件信息-->
+            <div id="thelist" class="uploader-list"></div>
+            <div class="btns">
+                <div id="picker">选择文件</div>
+                <button id="ctlBtn" class="btn btn-default">开始上传</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/webuploader.js"></script>
+<script type="text/javascript" src="js/upload.js"></script>
+</body>
+</html>
